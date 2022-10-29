f459512803a0f852634d05e638f24244a3bb3c54
Zakareya Alatoli <zakareya.m.alatoli@gmail.com>
2022-10-29 11:04:13 -0400

    Puzzle is randomly generated now

---
+++ frontend/src/minigames/missing-letters/LetterCypher.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import puzzles from './letterpuzzles.json'
+
+function LetterCypher(props) {
+    const [answer, setAnswer] = useState(""); //the player's answer
+
+    //Get a random puzzle
+    const randomIndex = Math.floor(Math.random() * (puzzles.length - 1));
+    const puzzle = puzzles[randomIndex];
+
+    return (
+        <div className="letter-cypher-window">
+            <h1>Find the encoded string in the following message:</h1>
+            <h1>{puzzle.message}</h1>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                alert(answer.toUpperCase() == puzzle.hidden ? "YOU GOT IT!" : "WRONG ANSWER, BUCKO!");
+            }}>
+                <input type="text" style={{borderWidth : "5px"}} onChange={(e) => setAnswer(e.target.value)}/>
+                <input type="submit" value={"GIVE ANSWER"} style={{borderWidth : "5px"}}/>
+            </form>
+            <hr />
+        </div>
+    )
+}
+
+export default LetterCypher
